823633dd6ea990b70afabadea3f46db979479b0d
Johnny McKenzie <jzmckenzie@dundee.ac.uk>
2018-04-13 18:18:28 +0100

    added html parsing to view method

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ def home():
 
 @app.route('/view', methods=['GET', 'POST'])
 def view():
+  #if users browse here by themselves, get them back to the homepage
   if flask.request.method == 'GET':
     return redirect('/')
   else:
     url = flask.request.values.get('url')
-    return render_template("view.html", url=url)
+    #not sure why the negation here works, but it just does, so I'll leave it :)
+    if not url.find("https://") or not url.find("http://"):
+      return render_template("view.html", url=url)
+    else :
+      url = "https://" + url
+      return render_template("view.html", url=url)
 
 if __name__ == '__main__':
   app.run()
