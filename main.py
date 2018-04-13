4f125b598fdb26d980e0fd78a2721da1b47b856b
Johnny McKenzie <jzmckenzie@dundee.ac.uk>
2018-04-13 18:05:31 +0100

    updated controller-views system

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from flask import Flask
+import bs4 as bs
+import flask
+
+from urllib.request import urlopen
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, Azure! :)'
+def home():
+  return render_template('home.html')
+
+@app.route('/view', methods=['GET', 'POST'])
+def view():
+  if flask.request.method == 'GET':
+    return redirect('/')
+  else:
+    url = flask.request.values.get('url')
+    return render_template("view.html", url=url)
 
 if __name__ == '__main__':
   app.run()
