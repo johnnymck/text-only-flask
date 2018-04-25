cf06593dd136036f02cab9106d97f57980cab44c
Johnny McKenzie <jzmckenzie@dundee.ac.uk>
2018-04-25 19:29:37 +0100

    changed url parsing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import bs4 as bs
 import flask
 import requests
 
+from urllib.parse import urlparse
 from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
@@ -15,19 +16,20 @@ def view():
   if flask.request.method == 'GET':
     return redirect('/')
   else:
-    url = flask.request.values.get('url')
-    #not sure why the negation here works, but it just does, so I'll leave it :)
-    if not url.find("https://") or not url.find("http://"):
-      return render_template("view.html", url=url)
-    else :
-      url = "https://" + url
-      html = requests.get(url).text
-      soup = bs.BeautifulSoup(html, 'html.parser')
-      body = soup.find('body')
-      all_el= body.findChildren()
-      [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
-      visible_text = soup.find_all(['h1', 'h2', 'h3', 'a','p', 'img'])
-      return render_template("view.html", html=visible_text)
+    url_raw = flask.request.values.get('url')
+    url = url_raw
+    if 'https' not in url:
+      if 'http' in url:
+        url.replace('http', 'https')
+      else:
+        url = 'https://' + url_raw
+    html = requests.get(url).text
+    soup = bs.BeautifulSoup(html, 'html.parser')
+    body = soup.find('body')
+    all_el= body.findChildren()
+    [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
+    visible_text = soup.find_all(['h1', 'h2', 'h3', 'a','p', 'img'])
+    return render_template("view.html", html=visible_text)
 
 if __name__ == '__main__':
   app.run()
